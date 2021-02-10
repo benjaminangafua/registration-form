0bc8f2b601b7b3f3d6a2581359a7d051f0391344
Benjamin A. Ngafua <benjaminarkutl2017@gmail.com>
2021-02-10 14:13:43 +0000

    Http server

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # httpserver-exercise
 Learning ground
+# The instructions say,
+
+Create a file called registration.json
+Create the following routes: registration, login and profile
+Create a file called registration.html
+In your registration.html, create a registration form with the following input fields: Full Name, username, email and password
+-Store the user registration data in registration.json
+Create a file called login.html
+-in your login.html file, create a login form with the following input fields: username and password
+Create a file for displaying user's profile called profile.html
+Show user profile if their username and password are found in the registration.json file
---
+++ httpserver.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require("fs");
+// let file = new StaticRange.server("./public")
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    if (req.url === '/' && req.method === "GET") {
+        fs.readFile('./registration.html', 'utf8', (err, data) => {
+            res.end(data);
+        })
+    } else if (req.url === '/login' && req.method === "GET") {
+        fs.readFile('./login.html', 'utf8', (err, data) => {
+            res.end(data)
+        })
+    } else if (req.url === '/profile' && req.method === "GET") {
+
+    } else if (req.url === '/registration' && req.method === "POST") {
+        let body = '';
+        req.on("data", (data) => {
+            body += data;
+        });
+        req.on("end", () => {
+            fs.readFile('./registration.json', 'utf8', (err, data) => {
+                var jsonBody = JSON.parse(data)
+                let front_end_data = JSON.parse(body)
+                jsonBody[front_end_data.username] = front_end_data
+
+                let jsonBody_string = JSON.stringify(jsonBody)
+                console.log(jsonBody_string)
+                fs.writeFile('./registration.json', jsonBody_string, err => {
+                    if (err) {
+                        console.error(err)
+                        return
+                    }
+                    //file written successfully
+                })
+            })
+
+        })
+
+    }
+})
+
+server.listen(3100, () => {
+    console.log('Listening on port 3100')
+})
