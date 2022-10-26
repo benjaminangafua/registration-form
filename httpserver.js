3da53d2abdc2f9f67d2a8c8c0fe092afc7fde574
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2022-10-26 14:16:37 +0000

    Updated and clean up

--- httpserver.js
+++ httpserver.js
@@ -1,35 +1,4 @@
-<<<<<<< HEAD
-const http = require('http'); //Require the http module to be able to access its library
-const fs = require("fs"); // Require file system module to access the file library in nodejs
 
-// create the server for running your application
-const server = http.createServer((req, res) => {
-
-    res.statusCode = 200; // The ok status of the document if no error
-    res.setHeader('Content-Type', 'text/html'); //The type of content file
-    // The home route 
-    if (req.url === '/' && req.method === "GET") {
-        fs.readFile('./registration.html', 'utf8', (err, data) => { // Reading the registration file path
-                res.end(data); // to be display the file the last parameter of the call back function
-            })
-            // The login route
-
-    } else if (req.url === '/login' && req.method === "GET") {
-        let login_body = '';
-        req.on("data", (data) => {
-            login_body += data;
-        })
-        fs.readFile('./login.html', 'utf8', (err, data) => {
-            res.end(data)
-        })
-
-        // Profile route
-    } else if (req.url === '/profile' && req.method === "GET") {
-        fs.readFile('./profile.html', 'utf8', (err, data) => {
-                res.end(data);
-            })
-            // registration route
-=======
 const http = require('http'); //importing http module
 const fs = require("fs"); //importing file system module
 let userdata; //global variable for hosting username with all the other data for registration.json file
@@ -79,7 +48,6 @@ const server = http.createServer((req, res) => { //creating server
                 res.end(data);
             })
             //Registation data from front-end
->>>>>>> 7061b53a9dfe3c2234a532733f8f81de506958b2
     } else if (req.url === '/registration' && req.method === "POST") {
         // Raw data from the form
         let body = '';
@@ -87,16 +55,6 @@ const server = http.createServer((req, res) => { //creating server
             body += data;
             console.log("What is this body?", body)
         });
-<<<<<<< HEAD
-        // Sending the html form value to the json
-        req.on("end", () => {
-            fs.readFile('./registration.json', 'utf8', (err, data) => {
-                var jsonBody = JSON.parse(data) // Form coming in vertical (json) form
-                let front_end_data = JSON.parse(body)
-                jsonBody[front_end_data.username] = front_end_data //Specifying the user as the owner of the form submitted
-
-                let jsonBody_string = JSON.stringify(jsonBody) // Passing json data to string
-=======
         // passing front-end data to json file
         req.on("end", () => {
             fs.readFile('./registration.json', 'utf8', (err, data) => {
@@ -107,7 +65,6 @@ const server = http.createServer((req, res) => { //creating server
                 jsonBody[front_end_data.username] = front_end_data; //specifying key for getting a registered user as "user"
 
                 let jsonBody_string = JSON.stringify(jsonBody) //The json value hosting the user info passing to string
->>>>>>> 7061b53a9dfe3c2234a532733f8f81de506958b2
                 console.log(jsonBody_string)
                 fs.writeFile('./registration.json', jsonBody_string, err => { //storing data in json file 
                     if (err) {
